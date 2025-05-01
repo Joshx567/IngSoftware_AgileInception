69b41da93b70f199aea14c55edf96217cd493db5
MarceloNaviaR <marcelo.navia@gmail.com>
2025-05-01 15:13:51 -0400

    Estaciones

---
+++ estaciones.js
@@ -0,0 +1,20 @@
+[
+    {
+        "id": 1,
+        "nombre": "Estación Central",
+        "ubicacion": "Av. Ayacucho 123",
+        "combustibles": {
+            "gasolina": { "disponible": true, "litros": 5000, "tiempoEspera": 30 },
+            "diesel": { "disponible": true, "litros": 3000, "tiempoEspera": 45 }
+        }
+    },
+    {
+        "id": 2,
+        "nombre": "Estación Norte",
+        "ubicacion": "Av. América 456",
+        "combustibles": {
+            "gasolina": { "disponible": false, "litros": 0, "tiempoEspera": 0 },
+            "diesel": { "disponible": true, "litros": 2000, "tiempoEspera": 60 }
+        }
+    }
+]
