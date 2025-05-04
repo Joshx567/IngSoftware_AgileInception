2ef69399da6fa82791caeabe62b3eed81e1467ce
Mateo Gandarillas <mateoganav@gmail.com>
2025-05-03 20:08:13 -0400

    feat: Actualizar disponibilidad cuando los litros o cantidad de combustible cambia >0

--- src/app.js
+++ src/app.js
@@ -14,10 +14,13 @@ export function registrarLitros(idEstacion, cantidad, tipoCombustible, operario)
   const cantidadIngresada = cantidad;
   if (tipoCombustible === "gasolina") {
     estacion.combustibles.gasolina.litros += cantidadIngresada;
+    estacion.combustibles.gasolina.disponible = true;
   } else if (tipoCombustible === "diesel") {
     estacion.combustibles.diesel.litros += cantidadIngresada;
+    estacion.combustibles.diesel.disponible = true;
   } else if (tipoCombustible === "gnv") {
     estacion.combustibles.gnv.litros += cantidadIngresada;
+    estacion.combustibles.gnv.disponible = true;
   } 
   historialIngresos.push({
     idEstacion,
