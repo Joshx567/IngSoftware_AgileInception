eff414347a6fd33670dd6aa789f35cf1241f1deb
Mateo Gandarillas <mateoganav@gmail.com>
2025-05-14 12:50:41 -0400

    refact: Se movieron a archivos separados las funciones registrar y generar tickets para mantener el código de forma más sencila

--- src/generarTicket.js
+++ src/generarTicket.js
@@ -1,39 +1,6 @@
 import {estacionesDB} from '../db.js';
 export const historialTickets = [];
 
-export const historialIngresos = [];
-
-export function registrarLitros(nombreEstacion, cantidad, tipoCombustible, operario) {
-  const estacion = estacionesDB.find(e => e.nombre === nombreEstacion);
-  if (!estacion){
-    throw new Error("La estacion ingresada no existe");
-  }
-  if (cantidad <= 0){
-    throw new Error("Cantidad ingresada invalida");
-  }
-  const cantidadIngresada = cantidad;
-  if (tipoCombustible === "gasolina") {
-    estacion.combustibles.gasolina.litros += cantidadIngresada;
-    estacion.combustibles.gasolina.disponible = true;
-  } else if (tipoCombustible === "diesel") {
-    estacion.combustibles.diesel.litros += cantidadIngresada;
-    estacion.combustibles.diesel.disponible = true;
-  } else if (tipoCombustible === "gnv") {
-    estacion.combustibles.gnv.litros += cantidadIngresada;
-    estacion.combustibles.gnv.disponible = true;
-  } 
-  historialIngresos.push({
-    idEstacion: estacion.id,
-    nombreEstacion,
-    tipoCombustible,
-    cantidadIngresada,
-    operario,
-    fecha:new Date().toLocaleDateString(),
-    hora: new Date().toLocaleTimeString()
-  });
-  return cantidadIngresada;
-}
-
 export function generarTicket(nombreEstacion, cantidad, tipoCombustible, operario, horaTicket) {
   const estacion = estacionesDB.find(e => e.nombre === nombreEstacion);
   if (!estacion) {
--- src/generarTicket.test.js
+++ src/generarTicket.test.js
@@ -1,69 +1,11 @@
-import { registrarLitros, generarTicket, historialIngresos, historialTickets} from './app.js';
+import { registrarLitros, historialIngresos} from './registrarLitros.js';
+import { generarTicket, historialTickets} from './generarTicket.js';
 import {estacionesDB} from "../db.js";
 
 
-describe("Registrar llegada de combustible", () => {
-    it("deberia registrar la llegada del combustible gasolina", () => {
-      expect(registrarLitros("Estación Principal",1000,'gasolina','Carlos')).toEqual(1000);
-    });
-    it("deberia registrar la llegada del combustible gnv", () => {
-      expect(registrarLitros("Estación Principal",1000,'gnv','Carlos')).toEqual(1000);
-    });
-    it("deberia registrar la llegada del combustible diesel", () => {
-      expect(registrarLitros("Estación Principal",1000,'diesel','Carlos')).toEqual(1000);
-    });
-    it("debería rechazar el registro negativo de combustible", () => {
-      try {
-        registrarLitros("Estación Principal", -100, "gasolina", "Carlos");
-      } catch (error) {
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toBe("Cantidad ingresada invalida");
-      }
-    });
-    it("debería rechazar el registro igual a cero de combustible", () => {
-      try {
-        registrarLitros("Estación Principal", 0, "gnv", "Carlos");
-      } catch (error) {
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toBe("Cantidad ingresada invalida");
-      }
-    });
-    it("debería rechazar el registro de una estación inexistente", () => {
-      try {
-        registrarLitros("Estación A", 12, "gasolina", "Carlos");
-      } catch (error) {
-        expect(error).toBeInstanceOf(Error);
-        expect(error.message).toBe("La estacion ingresada no existe");
-      }
-    });
-});
-
-describe("Registrar llegada de combustible en un historial", () => {
-    it("deberia registrar la llegada del combustible en un historial", () => {
-    expect(historialIngresos.length).toEqual(3);
-    });
-});
-
-describe("Actualizar la cantidad de combustible de la estación", () => {
-    it("debería actualizar los litros de combustible de la Estación Principal", () => {
-      const estacion = estacionesDB.find(e => e.nombre === "Estación Principal");
-      expect(estacion.combustibles.gasolina.litros).toEqual(1000);
-      expect(estacion.combustibles.gnv.litros).toEqual(1000);
-      expect(estacion.combustibles.diesel.litros).toEqual(1000);
-    });
-});
-
-describe("Verificar la disponibilidad del combustible", () => {
-  it("deberia regresar true si el combustible esta disponible", () => {
-    const estacion = estacionesDB.find(e => e.nombre === "Estación Principal");
-    expect(estacion.combustibles.gasolina.disponible).toBe(true);
-    expect(estacion.combustibles.diesel.disponible).toBe(true);
-    expect(estacion.combustibles.gnv.disponible).toBe(true);
-  });
-});
-
 describe("Generar tickets de combustible", () => {
   it("debería generar un ticket válido para una estación existente", () => {
+    registrarLitros("Estación Principal",1000,'gnv','Carlos');
     const ticket = generarTicket("Estación Principal", 30,"gnv","Carlos","10:00"); 
     expect(ticket).toHaveProperty("idTicket");
     expect(ticket).toHaveProperty("nombreEstacion","Estación Principal");
