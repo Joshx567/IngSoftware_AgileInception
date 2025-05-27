3b7a6bd41e78a2286e2d6165ac444ac12b3589af
Mateo Gandarillas <mateoganav@gmail.com>
2025-05-26 20:33:35 -0400

    feat: Se resta el monto ingresado del total del ticket de combustible

--- src/sacarTicket.js
+++ src/sacarTicket.js
@@ -3,5 +3,9 @@ import {estacionesDB} from "../db.js";
 export function sacarTicket(monto, estacionSeleccionada, idTicketSeleccionado) {
     let estacion = estacionesDB.find(e => e.nombre === estacionSeleccionada);
     let ticket = estacion.ticketsCombustible.find(t => t.idTicket === idTicketSeleccionado);
+    if (ticket.cantidadIngresada < monto) {
+        throw new Error("No hay suficiente combustible disponible");
+    }
+    ticket.cantidadIngresada -= monto;
     return monto;
 }
--- src/sacarTicket.test.js
+++ src/sacarTicket.test.js
@@ -4,7 +4,7 @@ import {estacionesDB} from "../db.js";
 
 describe("Sacar un ticket para un turno de una estación", () => {
     it("deberia registrar el ticket", () => {  
-    expect(sacarTicket(5,'Estación Norte')).toEqual(5);
+    expect(sacarTicket(5,'Estación Norte',2)).toEqual(5);
     });
     it("deberia encontrar la estación seleccionada para el ticket", () => {  
     const estacion = estacionesDB.find(e => e.nombre === 'Estación Norte');
@@ -15,5 +15,20 @@ describe("Sacar un ticket para un turno de una estación", () => {
     let estacion = estacionesDB.find(e => e.nombre === 'Estación Norte');
     const ticket = estacion.ticketsCombustible.find(t => t.idTicket === 2);
     expect(ticket.tipoCombustible).toEqual('diesel');
+    });
+    it("debería rechazar el ticket si el monto supera a la cantidad restante", () => {
+        try {
+          sacarTicket(500,"Estación Norte",2);
+          console.log('Se pasó la validación');
+        } catch (error) {
+          expect(error).toBeInstanceOf(Error);
+          expect(error.message).toBe("No hay suficiente combustible disponible");
+        }
+      });
+    it("deberia reducir la cantida disponible del ticket de combustible de la estacion", () => {  
+    sacarTicket(5,'Estación Norte',2)
+    let estacion = estacionesDB.find(e => e.nombre === 'Estación Norte');
+    const ticket = estacion.ticketsCombustible.find(t => t.idTicket === 2);
+    expect(ticket.cantidadIngresada).toEqual(190);
     });
 });
