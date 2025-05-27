2ae51ccf4651ae3ace6dde6f49d62e0904b8b7f7
Mateo Gandarillas <mateoganav@gmail.com>
2025-05-26 20:48:44 -0400

    feat: Datos: nombre, placa, carnet  y hora añadidos al ticket

--- src/sacarTicket.js
+++ src/sacarTicket.js
@@ -1,14 +1,24 @@
 import {estacionesDB} from "../db.js";
 
-export function sacarTicket(montoIngresado, estacionSeleccionada, idTicketSeleccionado) {
+export function sacarTicket(nombreConductor, placaIngresada, carnet, montoIngresado, estacionSeleccionada, idTicketSeleccionado) {
     let estacion = estacionesDB.find(e => e.nombre === estacionSeleccionada);
     let ticket = estacion.ticketsCombustible.find(t => t.idTicket === idTicketSeleccionado);
     if (ticket.cantidadIngresada < montoIngresado) {
         throw new Error("No hay suficiente combustible disponible");
     }
     ticket.cantidadIngresada -= montoIngresado;
-
+    const conductores = ticket.ticketsConductores.length;
+    const [hora, minutos, segundos] = ticket.hora.split(":").map(Number);
+    const baseDate = new Date();
+    baseDate.setHours(hora);
+    baseDate.setMinutes(minutos + conductores * 10);
+    baseDate.setSeconds(segundos);
+    const horaAtencion = baseDate.toTimeString().split(' ')[0];
     ticket.ticketsConductores.push({
+        nombre: nombreConductor,
+        ci: carnet,
+        placa: placaIngresada,
         monto: montoIngresado,
+        horaAtencion: horaAtencion
     });
 }
