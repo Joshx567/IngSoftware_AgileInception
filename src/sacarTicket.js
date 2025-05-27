2dea6774811bd7e3fa6ce8d8d6262158e9b622a6
Mateo Gandarillas <mateoganav@gmail.com>
2025-05-26 20:22:55 -0400

    feat: Se encuentra el ticket deseado dentro de la estacion seleccionada

--- src/sacarTicket.js
+++ src/sacarTicket.js
@@ -1,6 +1,7 @@
 import {estacionesDB} from "../db.js";
 
-export function sacarTicket(monto, estacionSeleccionada) {
-    const estacion = estacionesDB.find(e => e.nombre === estacionSeleccionada);
+export function sacarTicket(monto, estacionSeleccionada, idTicketSeleccionado) {
+    let estacion = estacionesDB.find(e => e.nombre === estacionSeleccionada);
+    let ticket = estacion.ticketsCombustible.find(t => t.idTicket === idTicketSeleccionado);
     return monto;
 }
--- src/sacarTicket.test.js
+++ src/sacarTicket.test.js
@@ -4,11 +4,16 @@ import {estacionesDB} from "../db.js";
 
 describe("Sacar un ticket para un turno de una estación", () => {
     it("deberia registrar el ticket", () => {  
-    expect(sacarTicket(5)).toEqual(5);
+    expect(sacarTicket(5,'Estación Norte')).toEqual(5);
     });
     it("deberia encontrar la estación seleccionada para el ticket", () => {  
-    let estacion = estacionesDB.find(e => e.nombre === 'Estación Norte');
+    const estacion = estacionesDB.find(e => e.nombre === 'Estación Norte');
     expect(estacion.nombre).toEqual('Estación Norte');
     console.log(estacion);
+    });
+    it("deberia encontrar el ticket de combustible de la estacion", () => {  
+    let estacion = estacionesDB.find(e => e.nombre === 'Estación Norte');
+    const ticket = estacion.ticketsCombustible.find(t => t.idTicket === 2);
+    expect(ticket.tipoCombustible).toEqual('diesel');
     });
 });
