6cc915401d9f41f6a616971fd19f5606c0594346
Mateo Gandarillas <mateoganav@gmail.com>
2025-05-26 20:11:37 -0400

    feat: Ingresar una estacion para sacar ticket

--- src/sacarTicket.js
+++ src/sacarTicket.js
@@ -1,5 +1,6 @@
 import {estacionesDB} from "../db.js";
 
-export function sacarTicket(monto) {
+export function sacarTicket(monto, estacionSeleccionada) {
+    const estacion = estacionesDB.find(e => e.nombre === estacionSeleccionada);
     return monto;
 }
--- src/sacarTicket.test.js
+++ src/sacarTicket.test.js
@@ -3,7 +3,12 @@ import { sacarTicket } from './sacarTicket.js';
 import {estacionesDB} from "../db.js";
 
 describe("Sacar un ticket para un turno de una estación", () => {
-    it("deberia registrar el ticket", () => {
+    it("deberia registrar el ticket", () => {  
     expect(sacarTicket(5)).toEqual(5);
+    });
+    it("deberia encontrar la estación seleccionada para el ticket", () => {  
+    let estacion = estacionesDB.find(e => e.nombre === 'Estación Norte');
+    expect(estacion.nombre).toEqual('Estación Norte');
+    console.log(estacion);
     });
 });
